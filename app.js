9fd5f8e7ddd5cb725f8e3f7b417d11e2fe2667db
pranee525 <praneeth5252@gmail.com>
2026-03-22 19:41:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 8001;
 const MONGODB_URI = process.env.MONGODB_URI;
-//managed connection string
+
 // Middleware
 app.use(cors());
 app.use(express.json());
